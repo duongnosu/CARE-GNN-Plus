447594976391762659f011ea86da5acf57c783da
Nhu Duong <duonghoangnhu@outlook.com>
2025-06-07 20:12:02 +1000

    comment out Yelp

--- data_process.py
+++ data_process.py
@@ -10,16 +10,16 @@ if __name__ == "__main__":
 
 	prefix = 'data/'
 
-	yelp = loadmat('data/YelpChi.mat')
-	net_rur = yelp['net_rur']
-	net_rtr = yelp['net_rtr']
-	net_rsr = yelp['net_rsr']
-	yelp_homo = yelp['homo']
+	# yelp = loadmat('data/YelpChi.mat')
+	# net_rur = yelp['net_rur']
+	# net_rtr = yelp['net_rtr']
+	# net_rsr = yelp['net_rsr']
+	# yelp_homo = yelp['homo']
 
-	sparse_to_adjlist(net_rur, prefix + 'yelp_rur_adjlists.pickle')
-	sparse_to_adjlist(net_rtr, prefix + 'yelp_rtr_adjlists.pickle')
-	sparse_to_adjlist(net_rsr, prefix + 'yelp_rsr_adjlists.pickle')
-	sparse_to_adjlist(yelp_homo, prefix + 'yelp_homo_adjlists.pickle')
+	# sparse_to_adjlist(net_rur, prefix + 'yelp_rur_adjlists.pickle')
+	# sparse_to_adjlist(net_rtr, prefix + 'yelp_rtr_adjlists.pickle')
+	# sparse_to_adjlist(net_rsr, prefix + 'yelp_rsr_adjlists.pickle')
+	# sparse_to_adjlist(yelp_homo, prefix + 'yelp_homo_adjlists.pickle')
 
 	amz = loadmat('data/Amazon.mat')
 	net_upu = amz['net_upu']
